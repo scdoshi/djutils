d1370a676f82227c379c48e275d999bef018a8a4
Sid Doshi <sid@viacycle.com>
2012-11-05 02:53:40 -0500

    Add html5.py for html5 widget types

---
+++ djutils/html5.py
@@ -0,0 +1,32 @@
+###############################################################################
+## Imports
+###############################################################################
+# Django
+from djangi.forms.widgets import TextInput, DateInput, DateTimeInput, TimeInput
+
+
+###############################################################################
+## Widgets
+###############################################################################
+class EmailInput(TextInput):
+    input_type = 'email'
+
+
+class NumberInput(TextInput):
+    input_type = 'number'
+
+
+class TelephoneInput(TextInput):
+    input_type = 'tel'
+
+
+class DateInput(DateInput):
+    input_type = 'date'
+
+
+class DateTimeInput(DateTimeInput):
+    input_type = 'datetime'
+
+
+class TimeInput(TimeInput):
+    input_type = 'time'
